45f1e14de4bb49bc5eb0e7526f42dadb80bca6be
Chao Ching Tan <tcc25@np.edu.sg>
2022-07-17 14:52:38 +0800

    Added example of using variables

--- 03-single-webserver-variables/main.tf
+++ 03-single-webserver-variables/main.tf
@@ -28,8 +28,8 @@ resource "aws_security_group" "webserver-sg" {
   name = "webserver-sg"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = var.http_port
+    to_port     = var.http_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- 03-single-webserver-variables/variables.tf
+++ 03-single-webserver-variables/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "http_port" {
+  description = "Port of HTTP server"
+  type        = number
+  default     = 80
+}
