538b06d7b54213399fbab7d6cd5b1d29ddf16ed3
Chao Ching Tan <tcc25@np.edu.sg>
2022-07-17 13:28:51 +0800

    Added security group to allow SSH access

--- 02-single-webserver/main.tf
+++ 02-single-webserver/main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 resource "aws_instance" "webserver" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  #security_groups = [aws_security_group.webserver-sg.id,aws_security_group.ssh-sg.id]
+  vpc_security_group_ids = [ aws_security_group.webserver-sg.id, aws_security_group.ssh-sg.id ]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -28,6 +29,17 @@ resource "aws_security_group" "webserver-sg" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssh-sg" {
+  name = "ssh-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
